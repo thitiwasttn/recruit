760a9e44cf27d3bd5421c1bf9f282739bf462084
GA007007\Admin <thitiwas.n@g-able.com>
2021-10-11 16:10:30 +0700

    first

---
+++ src/main/java/com/thitiwas/recruit/recruit/RecruitApplication.java
@@ -0,0 +1,13 @@
+package com.thitiwas.recruit.recruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitApplication.class, args);
+	}
+
+}
